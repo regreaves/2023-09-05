b0ef654cee58d2ac87c98800a92edf54e4666cc2
Ralph Greaves <rgreaves@ycp.edu>
2023-09-05 20:41:34 -0400

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -1,6 +1,5 @@
 export const config = {
   notes: [ { content: 'Hello, world!' }, { content: 'Goodbye, world!' } ],
-//  notes: [ ],
   port: parseInt(process.env.PORT, 10) || 3000,
   'view engine': process.env.VIEW_ENGINE || 'pug'
 };
